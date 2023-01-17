7580781fb9b90cc9fa4be96dce168f36092ab315
Apenmun <apenmun@gmail.com>
2023-01-17 22:02:09 +0100

    <Starting addPost>

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,40 @@
 <template>
-    <Header></Header>
- 
-    <div>
-        this is home - 
-    </div>
+  <Header></Header>
 
-    
+  <p
+    class="my-3 grid place-content-center underline decoration-red-400 text-white bg-teal-300 rounded-md py-2 px-4"
+  >
+    Hello {{ user.user?.email }} !
+  </p>
+  <form @submit.prevent="handleSubmit" class=" bg-gray-600 bg-opacity-29 py-8 px-6 rounded-md"
+  style="width: 50%; margin: 0 auto">
+    <input class="my-2 bg-white rounded-md border-gray-400 focus:outline-none focus:shadow-outline-blue appearance-none block w-full px-3 py-2 leading-tight text-gray-700" type="text" placeholder="Title" v-model="title" />
+    <button class="flex items-center justify-between text-turquoise bg-emerald-100 rounded-md py-2 px-4" type="submit">Create task</button>
+  </form>
+  this is home
 </template>
 
 <script setup>
-import Header from '../components/Header.vue';
+import Header from "../components/Header.vue";
+import { useUserStore } from "../store/user";
+import { useTaskStore } from "../store/task";
+import { ref } from "vue";
+
+//import { useRouter } from 'vue-router';
+//import { supabase } from '../supabase';
+
+const user = useUserStore();
+const taskStore = useTaskStore();
+
+taskStore.fetchTasks();
+
+const title = ref("");
+
+const handleSubmit = () => {
+    console.log('formulario')
+}
+
+//119. Agregar Documentos 5:55
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/store/task.js
+++ src/store/task.js
@@ -9,13 +9,27 @@ export const useTaskStore = defineStore("tasks", {
   }),
   actions: {
     async fetchTasks() {
+      try {
       const { data: tasks } = await supabase
         .from("tasks")
         .select("*")
         .order("id", { ascending: false });
       this.tasks = tasks;
+    } catch (error){
+      console.log(error);
+    }
     },
     // Hacer POST
+    async addPost(user_id, title){
+      try {
+        const {error} = await supabase
+        .from("tasks")
+        .insert(user_id, title);
+        this.fetchTasks()
+      } catch (error) {
+        console.log(error)
+      }
+    }
     // Hacer el PUT (edit)
     // Hacer el delete
     // Hacer el PUT (cambiar entre completada y pendiente)
