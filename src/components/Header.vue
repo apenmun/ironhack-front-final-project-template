673a8814bfcc1e9e251945bd578b9eb120abd740
Apenmun <apenmun@gmail.com>
2023-01-31 20:07:03 +0100

    <Finalizado para Netlify>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         <span class="sr-only">Your Company</span>
         <img
           class="h-8 w-auto sm:h-20"
-          src="../img/DALL·E 2023-01-06 10.27.44.png"
+          src="../assets/DALL·E 2023-01-06 10.27.44.png"
           alt=""
         />
       </a>
